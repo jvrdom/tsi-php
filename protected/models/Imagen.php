af798b0e789e026ff9acc2d3c9d15bf8c6e48184
Javier Dominguez <jvrdom@gmail.com>
2014-06-06 20:56:17 -0300

    Agrego plugin de Jquery para le upload de imagenes

---
+++ protected/models/Imagen.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This is the model class for table "imagen".
+ *
+ * The followings are the available columns in table 'imagen':
+ * @property integer $id_imagen
+ * @property string $url
+ * @property integer $inmueble_id_inmueble
+ *
+ * The followings are the available model relations:
+ * @property Inmueble $inmuebleIdInmueble
+ */
+class Imagen extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'imagen';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('inmueble_id_inmueble', 'required'),
+			array('inmueble_id_inmueble', 'numerical', 'integerOnly'=>true),
+			array('url', 'length', 'max'=>45),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id_imagen, url, inmueble_id_inmueble', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'inmuebleIdInmueble' => array(self::BELONGS_TO, 'Inmueble', 'inmueble_id_inmueble'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id_imagen' => 'Id Imagen',
+			'url' => 'Url',
+			'inmueble_id_inmueble' => 'Inmueble Id Inmueble',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id_imagen',$this->id_imagen);
+		$criteria->compare('url',$this->url,true);
+		$criteria->compare('inmueble_id_inmueble',$this->inmueble_id_inmueble);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Imagen the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -16,44 +16,43 @@
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />-->
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/css/jquery.fileupload.css" />
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
-
-<?php
-$this->widget(
-    'booster.widgets.TbNavbar',
-        array(
-            'type' => 'inverse',
-            'brand' => 'TSI-PHP',
-            'brandUrl' => '#',
-            'collapse' => true, // requires bootstrap-responsive.css
-            'fixed' => 'top',
-            'fluid' => true,
-            'items' => array(
+    <?php
+        $this->widget(
+            'booster.widgets.TbNavbar',
                 array(
-                    'class' => 'booster.widgets.TbMenu',
-                    'type' => 'navbar',
+                    'type' => 'inverse',
+                    'brand' => 'TSI-PHP',
+                    'brandUrl' => '#',
+                    'collapse' => true, // requires bootstrap-responsive.css
+                    'fixed' => 'top',
+                    'fluid' => true,
                     'items' => array(
-                        array('label' => 'Home', 'url' => 'http://localhost/tsi-php/index.php', 'active' => true),
-                        array('label'=> 'Permisos', 'url'=>array('/rights'),'visible' => Yii::app()->user->checkAccess('Admin')),
+                        array(
+                            'class' => 'booster.widgets.TbMenu',
+                            'type' => 'navbar',
+                            'items' => array(
+                                array('label' => 'Home', 'url' => 'http://localhost/tsi-php/index.php', 'active' => true),
+                                array('label'=> 'Permisos', 'url'=>array('/rights'),'visible' => Yii::app()->user->checkAccess('Admin')),
+                            ),
+                        ),
+                        array(
+                            'class' => 'booster.widgets.TbMenu',
+                            'type' => 'navbar',
+                            'htmlOptions' => array('class' => 'pull-right'),
+                            'items' => array(
+                                array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
+                                array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+                            ),
+                        ),
                     ),
-                ),
-                array(
-                    'class' => 'booster.widgets.TbMenu',
-                    'type' => 'navbar',
-                    'htmlOptions' => array('class' => 'pull-right'),
-                    'items' => array(
-                        array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
-                        array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-                    ),
-                ),
-            ),
-        )
-);
-
-?>
+                )
+        );
+    ?>
 <div class="container">
   <?php echo $content; ?>
 </div>
@@ -68,7 +67,28 @@ $this->widget(
 </div><!-- footer -->
 
 <!-- Javascript -->
-  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/vendor/jquery.ui.widget.js"></script>
+    <script src="http://blueimp.github.io/JavaScript-Load-Image/js/load-image.min.js"></script>
+    <script src="http://blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
+    <!-- The Canvas to Blob plugin is included for image resizing functionality -->
+    <script src="http://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+    <script src="http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.iframe-transport.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-process.js"></script>
+    <!-- The File Upload image preview & resize plugin -->
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-image.js"></script>
+    <!-- The File Upload audio preview plugin -->
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-audio.js"></script>
+    <!-- The File Upload video preview plugin -->
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-video.js"></script>
+    <!-- The File Upload validation plugin -->
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-validate.js"></script>
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/jquery.fileupload-ui.js"></script>
+    <!-- The main application script -->
+    <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jQueryfileupload/js/main.js"></script>
+
 </body>
 </html>
