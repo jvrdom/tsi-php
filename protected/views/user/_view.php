628b91678493476db140b8512b8c7446f57e9b8d
Javier Dominguez <jvrdom@gmail.com>
2014-06-22 21:59:36 -0300

    CRUD de Usuarios

--- protected/views/user/_view.php
+++ protected/views/user/_view.php
@@ -1,16 +1,11 @@
 <div class="view">
 
-		<b><?php echo CHtml::encode($data->getAttributeLabel('id_usuario')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_usuario')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->id_usuario),array('view','id'=>$data->id_usuario)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
 	<?php echo CHtml::encode($data->username); ?>
 	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
-	<?php echo CHtml::encode($data->password); ?>
-	<br />
-
 
 </div>
